e8c34f243623d4a49892c1974756b1753ee87274
Gabriel <gabrielgamboav19@gmail.com>
2022-02-05 13:36:29 -0600

    Succesfully proxying to bite

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,43 @@
-use tokio::{net::TcpStream, io::{AsyncWriteExt, AsyncReadExt}};
 use std::error::Error;
 use tokio::time::{sleep, Duration};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
 // use tokio::sync::mpsc;
 // use warp::{http::StatusCode, reply::json, ws::Message, Reply};
-use warp::{Reply, Filter, Rejection};
+use futures::channel::mpsc;
 use futures::{FutureExt, StreamExt};
 use warp::ws::{Message, WebSocket};
-use futures::channel::mpsc;
+use warp::{Filter, Rejection, Reply};
 
 type Result<T> = std::result::Result<T, Rejection>;
 
-pub async fn client_connection(ws: WebSocket)  {
+pub async fn client_connection(ws: WebSocket) {
     let (client_ws_sender, mut client_ws_rcv) = ws.split();
-    
+
     let mut stream = TcpStream::connect("127.0.0.1:1984").await.unwrap();
     let (mut byte_rx, mut byte_tx) = stream.into_split();
-    
+
     let (client_sender, client_rcv) = mpsc::unbounded();
-    
+
     // check for new messages from Bite
-    tokio::task::spawn(async move { 
+    tokio::task::spawn(async move {
         loop {
             let mut b2 = [0; 100];
-            byte_rx.readable().await;
+            let _success = byte_rx.readable().await;
 
             //Saves bite's response on b2 buffer
             let p = byte_rx.try_read(&mut b2);
-            
+
             match p {
                 Ok(_i) => {
                     let s = String::from_utf8_lossy(&b2);
-                    client_sender.unbounded_send(Ok(Message::text(s)));
-                },
-                Err(e) => continue,
+                    let _success = client_sender.unbounded_send(Ok(Message::text(s)));
+                }
+                Err(_e) => continue,
             };
-    
         }
-
     });
 
     tokio::task::spawn(client_rcv.forward(client_ws_sender).map(|result| {
@@ -46,9 +47,9 @@ pub async fn client_connection(ws: WebSocket)  {
     }));
 
     println!("{}", "connected");
+    
     // Check for new messages from the Client
     while let Some(result) = client_ws_rcv.next().await {
-        let b2 = [0; 100];
         let msg = match result {
             Ok(msg) => msg,
             Err(e) => {
@@ -56,12 +57,18 @@ pub async fn client_connection(ws: WebSocket)  {
                 break;
             }
         };
-        //write to bite 
-        byte_tx.writable().await;
-        let n = byte_tx.try_write(&msg.into_bytes()).unwrap();
+        //write to bite
+        let _success = byte_tx.writable().await;
+
+        let n_bytes = byte_tx.try_write(&msg.into_bytes());
+
+        match n_bytes {
+            Ok(_i) => continue,
+            Err(e) => println!("Unable to write {}", e),
+        };
     }
- 
-    //disconnects user 
+
+    //disconnects user
     println!("{}", "disconnected");
 }
 
@@ -73,15 +80,11 @@ pub async fn ws_handler(ws: warp::ws::Ws) -> Result<impl Reply> {
         None => Err(warp::reject::not_found()),
     }
 }
+
 #[tokio::main]
 async fn main() {
-    
-    let ws_route = warp::path("ws")
-    .and(warp::ws())
-    .and_then(ws_handler);
-    
+    let ws_route = warp::path("ws").and(warp::ws()).and_then(ws_handler);
+
     println!("{}", "listening on port 8000");
     warp::serve(ws_route).run(([127, 0, 0, 1], 8000)).await;
-
 }
-
