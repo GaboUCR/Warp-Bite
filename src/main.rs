2ee1b82ba43fd64ad85c0246dfdf22af9ffbefec
Gabriel <gabrielgamboav19@gmail.com>
2022-02-06 05:09:36 -0600

    Client can disconnect safely

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::error::Error;
+use futures::future::ok;
 use tokio::time::{sleep, Duration};
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::TcpStream,
 };
-// use tokio::sync::mpsc;
-// use warp::{http::StatusCode, reply::json, ws::Message, Reply};
+use std::io::ErrorKind::{BrokenPipe, Interrupted, WouldBlock};
+
 use futures::channel::mpsc;
 use futures::{FutureExt, StreamExt};
 use warp::ws::{Message, WebSocket};
@@ -22,7 +23,7 @@ pub async fn client_connection(ws: WebSocket) {
     let (client_sender, client_rcv) = mpsc::unbounded();
 
     // check for new messages from Bite
-    tokio::task::spawn(async move {
+    let bite_read_handler = tokio::task::spawn(async move {
         loop {
             let mut b2 = [0; 100];
             let _success = byte_rx.readable().await;
@@ -31,42 +32,69 @@ pub async fn client_connection(ws: WebSocket) {
             let p = byte_rx.try_read(&mut b2);
 
             match p {
-                Ok(_i) => {
+                Ok(i) => {
+                    // Reading 0 bytes means the other side has closed the
+                    // connection or is done writing, then so are we.
+                    if i == 0 {
+                        break;
+                    }
                     let s = String::from_utf8_lossy(&b2);
                     let _success = client_sender.unbounded_send(Ok(Message::text(s)));
                 }
-                Err(_e) => continue,
+                Err(ref err) if err.kind() == WouldBlock => continue,
+
+                Err(ref err) if err.kind() == Interrupted => continue,
+
+                // Other errors we'll consider fatal.
+                Err(_err) => break,
             };
         }
     });
 
-    tokio::task::spawn(client_rcv.forward(client_ws_sender).map(|result| {
+    let bite_proxy = tokio::task::spawn(client_rcv.forward(client_ws_sender).map(|result| {
+        //this task is ended when bite_read_handler ends
         if let Err(e) = result {
             eprintln!("error sending websocket msg: {}", e);
         }
     }));
+    // Check for new messages from the Client
+    let client_handler = tokio::task::spawn(async move {
+        while let Some(result) = client_ws_rcv.next().await {
+            let msg = match result {
+                Ok(msg) => msg,
+                //should this close the connection??
+                Err(e) => {
+                    eprintln!("error receiving ws message {}", e);
+                    break;
+                }
+            };
+            //write to bite
+            let _success = byte_tx.writable().await;
+
+            let n_bytes = byte_tx.try_write(&msg.into_bytes());
+
+            match n_bytes {
+                Ok(_i) => continue,
+                Err(e) => println!("Unable to write {}", e),
+            };
+        }
+    });
 
     println!("{}", "connected");
-    
-    // Check for new messages from the Client
-    while let Some(result) = client_ws_rcv.next().await {
-        let msg = match result {
-            Ok(msg) => msg,
-            Err(e) => {
-                eprintln!("error receiving ws message {}", e);
-                break;
-            }
-        };
-        //write to bite
-        let _success = byte_tx.writable().await;
 
-        let n_bytes = byte_tx.try_write(&msg.into_bytes());
-
-        match n_bytes {
-            Ok(_i) => continue,
-            Err(e) => println!("Unable to write {}", e),
-        };
+    //If any task closes every other task needs to close
+    tokio::select! {
+        _ = client_handler => {
+            println!("The websocket was disconnected");
+        }
+        _ = bite_proxy => {
+            println!("bite proxy got disconnected");
+        }
+        _ = bite_read_handler => {
+            println!("Bite got disconnected");
+        }
     }
+
 
     //disconnects user
     println!("{}", "disconnected");
